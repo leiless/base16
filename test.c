3e598f2cd8b4b2305cb5548fc7d393ef0ffebf5e
Lei <leixiang@nutstore.net>
2019-04-04 11:10:29 +0800

    Refine base16_encode2() test code

--- test.c
+++ test.c
@@ -14,10 +14,12 @@
 void generate_u32_table(void)
 {
     int i;
+    uint16_t t;
     char buff[5];
 
     for (i = 0; i <= USHRT_MAX; i++) {
-        base16_encode(buff, &i, sizeof(uint16_t));
+        t = htons((uint16_t) i);
+        base16_encode(buff, &t, sizeof(uint16_t));
         printf("%s,%c", buff, i % M != N ? ' ' : '\n');
         /* Machine endianness */
         //printf("%#x,%c", *((uint32_t *) buff), i % M != N ? ' ' : '\n');
@@ -31,6 +33,7 @@ void generate_u32_table(void)
 int main(void)
 {
     int i;
+    uint16_t t;
     char buff[3];
     char buff2[5];
 
@@ -47,7 +50,8 @@ int main(void)
     puts("");
 
     for (i = 0; i <= USHRT_MAX; i++) {
-        base16_encode2(buff2, &i, sizeof(uint16_t));
+        t = htons((uint16_t) i);
+        base16_encode2(buff2, &t, sizeof(uint16_t));
         printf("%s,%c", buff2, i % 16 != 15 ? ' ' : '\n');
     }
     puts("");
